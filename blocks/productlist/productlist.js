81af6e65520628f290e6238eb709858c0492e927
Lakshmi891 <lmamidi@adobe.com>
2025-02-27 17:02:56 +0530

    Update productlist.js

--- blocks/productlist/productlist.js
+++ blocks/productlist/productlist.js
@@ -34,7 +34,7 @@ export default function decorate(block) {
                         <h2 class="product-title">${product.productname}</h2>
                     </a>
                     <p class="product-price">${product.productprice}</p>
-                    <button class="add-to-cart">ADD TO CART</button>
+                    <button class="add-to-cart">ADD TO CAR</button>
                     <svg class="wishlist-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.6l-1-1a5.5 5.5 0 0 0-7.8 7.8l1 1 7.8 7.8 7.8-7.8 1-1a5.5 5.5 0 0 0 0-7.8z"></path>
                     </svg>
