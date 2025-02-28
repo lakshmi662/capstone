054945a9c2a3f18526579b38628a265eb2d0c9d1
Lakshmi891 <lmamidi@adobe.com>
2025-02-28 17:51:27 +0530

    Testing breadcrumb

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -1,5 +1,27 @@
+/* function generateBreadcrumbHTML(pagePath) {
+    const paths = pagePath.replace(/^\/|\/$/g, '').split('/');
+    let html = '<span class="breadcrumb-item"><a href="/">Home</a></span>';
+   
+    let currentHref = '';
+    paths.forEach((path, index) => {
+        if (path) {
+            currentHref += `/${path}`;
+            const isLast = index === paths.length - 1;
+           
+            if (isLast) {
+                html += `<span class="breadcrumb-separator">/</span><span class="breadcrumb-item">${path}</span>`;
+            } else {
+                html += `<span class="breadcrumb-separator">/</span><span class="breadcrumb-item"><a href="${currentHref}">${path}</a></span>`;
+            }
+        }
+    });
+   
+    return html;
+} */
+ 
 export default function decorate(block) {
-    console.log(block)
-    // const pagePath = window.location.pathname;
-    // block.innerHTML = generateBreadcrumbHTML(pagePath);
+    
+    const pagePath = window.location.pathname;
+    console.log(block, pagePath)
+    block.innerHTML = '<div>Shravani</div>';
 }
